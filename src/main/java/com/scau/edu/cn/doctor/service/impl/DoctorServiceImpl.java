f7d396612240a37bd5d4a8b462a35ee341da2310
heng <1187810825@qq.com>
2024-06-20 16:33:32 +0800

    login

--- src/main/java/com/scau/edu/cn/doctor/service/impl/DoctorServiceImpl.java
+++ src/main/java/com/scau/edu/cn/doctor/service/impl/DoctorServiceImpl.java
@@ -1,16 +1,21 @@
 package com.scau.edu.cn.doctor.service.impl;
 
-import com.baomidou.mybatisplus.core.conditions.AbstractLambdaWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.scau.edu.cn.doctor.domain.Doctor;
 import com.scau.edu.cn.doctor.service.DoctorService;
 import com.scau.edu.cn.doctor.mapper.DoctorMapper;
+import com.scau.edu.cn.doctor.util.JwtUtils;
 import com.scau.edu.cn.doctor.util.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import static com.scau.edu.cn.doctor.util.Status.USER_LOGIN_NOT_EXIST;
 import static com.scau.edu.cn.doctor.util.Status.USER_LOGIN_PASSWORD_ERROR;
@@ -26,6 +31,10 @@ public class DoctorServiceImpl extends ServiceImpl<DoctorMapper, Doctor>
 
     @Resource
     private DoctorMapper doctorMapper;
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+
     @Override
     public Result login(Doctor doctor) {
         Doctor doc = null;
@@ -44,7 +53,27 @@ public class DoctorServiceImpl extends ServiceImpl<DoctorMapper, Doctor>
                 return Result.error(USER_LOGIN_PASSWORD_ERROR);
             }
         }
-        return Result.success(doc);
+        //创建redis缓存,放token令牌
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("docId", doc.getDocId());
+        String token = JwtUtils.generateJwt(map);
+        // 准备一个HashMap来存储与JWT关联的额外信息到Redis
+        Map<String, String> tokenInfo = new HashMap<>();
+        tokenInfo.put("docId", doc.getDocId()); // 存储用户ID，便于根据token查找用户
+        tokenInfo.put("token", token); // 可选，也可以只存储token作为标识
+        tokenInfo.put("expiration", String.valueOf(System.currentTimeMillis() + 1800000L)); // 存储JWT的过期时间戳
+        // 将token作为键，关联信息作为值存入Redis
+        //设计这个键的时间长度，可以根据实际情况设置，比如15分钟，30分钟，1小时，1天等等
+
+        redisTemplate.opsForHash().putAll(token, tokenInfo);
+        redisTemplate.expire(token, 1800, TimeUnit.SECONDS);
+
+        Map<String, Object> result = new HashMap<>();
+        result.put("token", token);
+        result.put("doctor", doc);
+        System.out.println(result);
+        return Result.success(result);
     }
 }
 
