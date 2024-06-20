caec8eaedf91177eeae2ffe140a89ec82b9c8cdd
何杰鸣 <9759400+he-jieming@user.noreply.gitee.com>
2024-06-20 16:16:46 +0800

    配置类

---
+++ src/main/java/com/scau/edu/cn/doctor/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.scau.edu.cn.doctor.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+//    @Autowired
+//    private LoginInterceptor loginInterceptor;
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        registry.addInterceptor(new LocaleChangeInterceptor()).addPathPatterns("/**").excludePathPatterns("/users/**").excludePathPatterns("/templates/**");
+    }
+}
